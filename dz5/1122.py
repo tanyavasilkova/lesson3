ef897e79bb2e72da592772ff0275b17a4eee0fc1
tanyavasilkova <tatiana_wt@mail.ru>
2019-03-26 17:17:16 -0400

    5 commit

---
+++ dz5/1122.py
@@ -0,0 +1,9 @@
+total = 100
+
+i = 0
+while i < 5:
+    n = int(input('FFGhfh: '))
+    total = total - n
+    i = i + 1
+
+print("Осталось", total)
---
+++ old_dz/dz5/1122.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+print(datetime.now())
+# print(dir(datetime))
+import math
+dir(math)
